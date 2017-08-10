904dbca68c973164cdaab13ed8c83d4b7566ef40
dentov13 <tovstokhatko.denis@gmail.com>
2017-08-10 11:23:56 +0300

    app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import WeatherIcon from './components/WeatherIcon';
+import WeatherDetails from './components/WeatherDetails';
 import './App.css';
 
 class App extends Component {
+  state = {
+    icon: '',
+    time: 1,
+    city: '',
+    temperature: '',
+    weatherCode: '',
+    fetching: true
+  }
+
+  componentDidMount() {
+    this.fetchIP();
+  }
+
+  fetchWeatherData = city => {
+    const baseUrl = `http://api.openweathermap.org`;
+    const path = `/data/2.5/weather`;
+    const appId = `ff380fa0a26c755c7880e066db650f55`;
+    const query = `units=metric&lang=ru&appid=${appId}`;
+
+    fetch(`${baseUrl}${path}?q=${city}&${query}`)
+      .then(response => response.json())
+      .then(data => {
+        const date = new Date();
+        const time = date.getHours();
+
+        this.setState({
+          time,
+          city,
+          temperature: Math.round(data.main.temp),
+          weatherCode: data.weather[0].id,
+          fetching: false
+        });
+      })
+      .catch(error => console.error(error));
+  }
+  
+  fetchIP = () => {
+    fetch('//freegeoip.net/json/')
+      .then(response => response.json())
+      .then(({ city }) => this.fetchWeatherData(city))
+      .catch(error => console.log(error));
+  }
+
   render() {
+    const { fetching, icon, time, city, temperature, weatherCode } = this.state;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='app'>Загрузка...</div>
+      :
+      <div className='app' data-hour={time}>
+        <WeatherIcon
+          icon={icon}
+          weatherCode={weatherCode}
+          time={time}
+        />
+        <WeatherDetails
+          city={city}
+          temperature={temperature}
+        />
       </div>
     );
   }
